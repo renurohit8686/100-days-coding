b1bc6c565a0f826bb0699b0d4a133d9fe3dc889f
Megha dhiman <98514020+renurohit8686@users.noreply.github.com>
2026-09-15 23:17:22 +0530

    Create code71.c
    This program reads a matrix of integers from input and prints it.

---
+++ code71.c
@@ -0,0 +1,37 @@
+Q71: Read and print a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+Output 1:
+1 2
+3 4
+
+*/
+  #include <stdio.h>
+
+int main() {
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    // Reading the matrix
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Printing the matrix
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+    }
+
+    return 0;
+}
