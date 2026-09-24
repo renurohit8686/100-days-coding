fc50341bdb18d12d37a85a98cfe956e6d98f457e
Megha dhiman <98514020+renurohit8686@users.noreply.github.com>
2026-09-24 10:55:29 +0530

    Create code91.c

---
+++ code91.c
@@ -0,0 +1,32 @@
+Q91: Remove all vowels from a string.
+
+/*
+Sample Test Cases:
+Input 1:
+education
+Output 1:
+dctn
+
+*/
+  #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[100], result[100];
+    int j = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+        if (!(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')) {
+            result[j++] = str[i];
+        }
+    }
+    result[j] = '\0';
+
+    printf("Output: %s\n", result);
+    return 0;
+}
